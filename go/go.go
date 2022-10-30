50842d7eeeef72b9175f7b8faf4912c1bc989860
Rafael Rodriguez Martinez <rafaelrodmar@hotmail.com>
2022-10-30 19:57:59 +0100

    Update go.go

--- go/go.go
+++ go/go.go
@@ -7,7 +7,7 @@ import "strconv"  //Package strconv implements conversions to and from string re
 import "unicode"
 
 var PI float64 = 3.141592653589793
-var mimetypelist [20000]string  //pares son extensiones, impares son el tipo
+var mimetypelist [20000]string  //definition of array of strings
 
 func checkarray(a string, n int) int {
     for x:=0; x<n*2 ; x++ {
@@ -18,11 +18,52 @@ func checkarray(a string, n int) int {
     return -1
 }
 
+func min(a, b int) int {
+    if a < b {
+        return a
+    }
+    return b
+}
+
+func Reverse(s string) string {
+    runes := []rune(s)
+    for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+        runes[i], runes[j] = runes[j], runes[i]
+    }
+    return string(runes)
+}
+
+//insert value in array
+func insert(a []int, index int, value int) []int {
+    if len(a) == index { // nil or empty slice or after last element
+        return append(a, value)
+    }
+    a = append(a[:index+1], a[index:]...) // index < len(a)
+    a[index] = value
+    return a
+}
+
+//definition of node for singly-linked list
+type ListNode struct{
+	Val int
+	Next *ListNode
+}
+
+//definition of node for a binary tree
+type TreeNode struct{
+	Val int
+	Left *TreeNode
+	Right *TreeNode
+}
+
 func main(){
 	var auxString string
 	//fmt.Scan(&auxString) //get input
 	auxString = "text string"
 	fmt.Println(auxString)
+
+	node := new(ListNode) //definition of a new node in memory
+	var tNode []*TreeNode //array of tree node pointers
 
 	var aux int  //int definition
 	fmt.Println(aux)
@@ -81,6 +122,14 @@ func main(){
 	}
 	fmt.Println("")
 
+	//get the int value of a charcter from the string 'a' at index 'indexa'
+	digitA := int(byte(rune(a[indexa]) - '0'))
+	//convert int to string
+	stringInt := strconv.Itoa(intNumber)
+	//sort algorithm
+	var nums []int
+	sort.Ints(nums)
+
 	// Take substring of first word with runes.
 	// ... This handles any kind of rune in the string.
 	//runes := []rune(ROW)
@@ -103,4 +152,37 @@ func main(){
    it := strings.LastIndexAny( messageinbinary, "0")
    newext := string(messageinbinary[it+1:])
    fmt.Println(newext)
+
+
+   //create a map
+   m := make(map[byte]int)
+   //traverse map
+   for _, v := range m{
+	 //operation
+   }
+
+   //divide a string into array of words
+   words := strings.Fields(sentence)
+   //get a substring of len 3 of the first word
+   w := words[0][0:len(3)]
+
+   //concatenation of same array
+   nums = append(nums,nums...)
+
+   //convert binary number in string to int32
+   res, _ := strconv.ParseInt(ans,2,32)
+
+   // convert string to byte slice, append byte to slice, convert back to string
+   //append subsTable[message[i]] to string 'ans'
+   ans = string(append([]byte(ans), subsTable[message[i]]))
+
+   //make a slice with len and cap = nums size
+   sumsleft := make([]int, len(nums), len(nums))
+
+   //count character " " in string
+   spaces := strings.Count(sentences[i]," ")
+
+   //sort a slice by element of node
+   var vln []*ListNode
+   sort.Slice(vln, func(a, b int) bool { return vln[a].Val < vln[b].Val })
 }
